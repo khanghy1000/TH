56015fedca4eb6855d164e0b70c030e1b2745e54
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2022-04-25 08:12:00 +0700

    Update TH3-Bai3.cpp

--- TH3-Bai3.cpp
+++ TH3-Bai3.cpp
@@ -3,14 +3,6 @@
 #include <sstream>
 using namespace std;
 
-bool LaSoNguyenTo(int a) {
-	if (a < 2) return 0;
-	for (int i = 2; i <= sqrt(a); i++) {
-		if (a % i == 0) return 0;
-	}
-	return 1;
-}
-
 int main() {
 	int a = 0, b = 0;
 	cin >> a >> b;
