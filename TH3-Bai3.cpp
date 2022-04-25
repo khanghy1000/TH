c8e02c45133d41a22fbf9c0f18b2392e91cee2c6
khanghy1000 <46063255+khanghy1000@users.noreply.github.com>
2022-04-25 08:11:20 +0700

    Add files via upload

---
+++ TH3-Bai3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+bool LaSoNguyenTo(int a) {
+	if (a < 2) return 0;
+	for (int i = 2; i <= sqrt(a); i++) {
+		if (a % i == 0) return 0;
+	}
+	return 1;
+}
+
+int main() {
+	int a = 0, b = 0;
+	cin >> a >> b;
+	string str1 = to_string(a);
+	string str2 = to_string(b);
+
+	int sum1 = 0, sum2 = 0;
+
+	for (int i = 0; i < str1.size(); i++) {
+		stringstream obj;
+		int a = 0;
+		obj << str1[i];
+		obj >> a;
+		sum1 += a;
+	}
+
+	for (int i = 0; i < str2.size(); i++) {
+		stringstream obj;
+		int a = 0;
+		obj << str2[i];
+		obj >> a;
+		sum2 += a;
+	}
+
+	if (sum1 > sum2) {
+		cout << b << " " << a << endl;
+	}
+	else {
+		cout << a << " " << b << endl;
+	}
+
+	system("pause"); 
+	return 0;
+}
